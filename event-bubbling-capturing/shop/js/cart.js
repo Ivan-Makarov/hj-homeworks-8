8251dca458159a8cb17d0141513d814d3d63759d
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-06-20 23:09:56 +0300

    Задача 3

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const itemsList = document.querySelector('.items-list');
+
+function createItem(item) {
+    return {
+        title: item.dataset.title,
+        price: item.dataset.price
+    }
+}
+
+function addItem(click) {
+    if (click.target.classList.contains('add-to-cart')) {
+        addToCart(createItem(click.target));
+    }
+}
+
+itemsList.addEventListener('click', addItem);
