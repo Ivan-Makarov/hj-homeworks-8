d8c97cc0d46bb4318175bfd2b9a19918460baca8
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-06-20 22:50:31 +0300

    Задача 2

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    if (event.target.tagName === 'TH' && event.target.dataset.propName) {
+
+        if (event.target.dataset.dir != 1) {
+            event.target.dataset.dir = 1;
+        } else {
+            event.target.dataset.dir = -1;
+        }
+
+        this.dataset.sortBy = event.target.dataset.propName;
+
+        sortTable(this.dataset.sortBy, event.target.dataset.dir)
+    }
 }
